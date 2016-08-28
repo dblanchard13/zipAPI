62db2a1b3122783c022fe98f606ae589eff37223
dblanchard13 <davidb.prof@gmail.com>
2016-08-27 19:06:38 -0700

    Add cors middleware

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -2,9 +2,11 @@
 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 module.exports = (app) => {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
+  app.use(cors())
 };
